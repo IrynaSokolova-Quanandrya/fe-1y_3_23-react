11b18cf1716e27ffba47f1d1ee0bc7796f2daaf4
Iryna <irasokolova25@ukr.net>
2025-02-04 18:50:28 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
 import painting from './painting.json';
 import { PaintingList } from './components/PaintingList/PaintingList'
 import { ColorPicker } from './components/ColorPicker/ColorPicker.jsx'
-import {colorPickerOptions} from './colorOptions.jsx'
+import { colorPickerOptions } from './colorOptions.jsx'
+import { Alert } from './components/Alert/Alert.jsx';
 import './App.css';
 import style from './App.module.css'
+import { Container } from './components/Container.jsx';
 console.log(style);
 
 export function App() {
   return(
     <div>
-      {/* <h1 className={style.title} style={{ color: '#e7543a', }}>Main title</h1>
-      <PaintingList painting={painting} /> */}
-    
+      <Container>
+        Hello
+        <h1>Title</h1>
+        <PaintingList painting={painting} />     
+        {/* <ColorPicker options={colorPickerOptions}/> */}
+      </Container>
 
-
-     
-      <ColorPicker options={colorPickerOptions}/>
-      {/* <Alert text="Останнє попередження" type="warning"/>
-      <Alert text="Ура! Все ок!" type="success"/>
-      <Alert text="О жах - все пропало!" type="error"/> */}
+         <Container>
+        <Alert text="Останнє попередження" type="warning"/>
+        <Alert text="Ура! Все ок!" type="success"/>
+        <Alert text="О жах - все пропало!" type="error"/>
+      </Container>
+      
     </div>
   )
 }
