32cd495778582cec5a68abf93708ebb02f293d2d
Iryna <irasokolova25@ukr.net>
2025-02-16 18:46:29 +0200

    add files

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import {Header} from './Header/Header'
+import {Counter} from './Counter/Counter'
+
+
+export const App = () => {
+			return (
+			<div className='container'>
+				<Header/>
+				<Counter />
+			</div>
+		)
+}
+
