c63e44bbb0be125f10c90f01c2c727aa7b0a39fb
Iryna <irasokolova25@ukr.net>
2025-02-21 19:13:29 +0200

    Update Counter.jsx

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,34 +1,57 @@
-export const Counter = () => {
-	return (
-		<div className='position-absolute top-50 start-50 translate-middle'>
-			<div
-				className='card bg-dark text-white '
-				style={{ width: '600px' }}
-			>
-				<div className='card-body'>
-					<h5 className='card-title text-center fs-1'>Counter</h5>
-					<p
-						className='card-text  text-center'
-						style={{ fontSize: '80px' }}
-					>
-						0
-					</p>
-					<div className='d-flex justify-content-center px-5'>
-						<button
-							className='btn btn-outline-success me-5'
-							// onClick={handleClick}
+import { Component } from "react";
+
+// let counter = 0
+
+// button.addEventListener('click', (e) => {
+// 	counter +=1
+// })
+
+export class Counter extends Component {
+
+	state = {
+		counter: 0
+	}
+
+	 handleClick = (e) => {
+		 // this.state.counter += 1 - !!! ПОГАНО
+		 
+		 //коли потрібно просто перезаписати стейт
+		 //  this.setState({counter: 1}) 
+
+		 //коли потрібно змінити стейт від попереднього значення
+		 this.setState(prevState=>({counter: prevState.counter+=1}))
+	}
+
+	render() {
+		return (
+			<div className='position-absolute top-50 start-50 translate-middle'>
+				<div
+					className='card bg-dark text-white '
+					style={{ width: '600px' }}
+				>
+					<div className='card-body'>
+						<h5 className='card-title text-center fs-1'>Counter</h5>
+						<p
+							className='card-text  text-center'
+							style={{ fontSize: '80px' }}
 						>
-							<i className='bi bi-plus-circle fs-1'></i>
-						</button>
-						<button
-							className='btn  btn-outline-danger ms-5'
-							// onClick={handleClick}
-						>
-							<i className='bi bi-dash-circle fs-1'></i>
-						</button>
+							{this.state.counter}
+						</p>
+						<div className='d-flex justify-content-center px-5'>
+							<button onClick={this.handleClick}
+								className='btn btn-outline-success me-5'
+							>
+								<i className='bi bi-plus-circle fs-1'></i>
+							</button>
+							<button
+								className='btn  btn-outline-danger ms-5'
+							>
+								<i className='bi bi-dash-circle fs-1'></i>
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	)
+		)
+	}
 }
