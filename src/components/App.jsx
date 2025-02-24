1158c81e9820f4be80d489d14dc90cd33f6bfec3
Iryna <irasokolova25@ukr.net>
2025-02-24 13:55:52 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,23 @@
 import {Header} from './Header/Header'
 import {Counter} from './Counter/Counter'
+import { ColorPicker } from './ColorPicker/ColorPicker';
 
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
 
 export const App = () => {
 			return (
 			<div className='container'>
-				<Header/>
-				<Counter />
+				{/* <Header/> */}
+					{/* <Counter /> */}
+					{/* <ColorPicker options={colorPickerOptions}/> */}
+					
 			</div>
 		)
 }
