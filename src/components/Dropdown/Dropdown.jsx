71b7123ac60eac90497220689edf5522e91f637e
Iryna <irasokolova25@ukr.net>
2025-02-24 13:50:58 +0200

    add files

---
+++ src/components/Dropdown/Dropdown.jsx
@@ -0,0 +1,10 @@
+/* <div className="Dropdown">
+        <button
+          type="button"
+          className="Dropdown__toggle"
+        >
+          Open
+        </button>
+
+       <div className="Dropdown__menu">Выпадающее меню</div>
+      </div> */
