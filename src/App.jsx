66892a74088aa48243b4b3279e589a529391ddc3
Iryna <irasokolova25@ukr.net>
2025-08-11 13:36:31 +0300

    prepared

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { RouterProvider } from "react-router-dom";
-import { routing } from "./pages/router";
+
 
 export const App = () => {
-  return <RouterProvider router={routing} />;
+  return <></>;
 };
